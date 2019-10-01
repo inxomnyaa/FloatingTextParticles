5cd35b68bc55f758f547a6f7c7812228610a14bb
thebigsmileXD <thebigsmilexd@gmail.com>
2019-10-01 16:42:42 +0200

    v1.2.1 - Remove particles from client upon /reload (mass lag othervise because duplicates)

--- src/xenialdan/FloatingTextParticles/Loader.php
+++ src/xenialdan/FloatingTextParticles/Loader.php
@@ -7,54 +7,68 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use xenialdan\FloatingTextParticles\other\FakeFloatingTextParticle;
 
+class Loader extends PluginBase
+{
+    /** @var Loader */
+    private static $instance = null;
+    /** @var array */
+    public static $editing = [];
+    /** @var array */
+    public static $removing = [];
+    /** @var FakeFloatingTextParticle[] */
+    public static $particles = [];
 
-class Loader extends PluginBase{
-	/** @var Loader */
-	private static $instance = null;
-	/** @var array */
-	public static $editing = [];
-	/** @var array */
-	public static $removing = [];
-	/** @var FakeFloatingTextParticle[] */
-	public static $particles = [];
+    private static function resetConfig()
+    {
+        foreach (self::getInstance()->getConfig()->getAll(true) as $key) {
+            self::getInstance()->getConfig()->remove($key);
+        }
+    }
 
-	private static function resetConfig(){
-		foreach (self::getInstance()->getConfig()->getAll(true) as $key){
-			self::getInstance()->getConfig()->remove($key);
-		}
-	}
+    public function onLoad()
+    {
+        self::$instance = $this;
+        $this->saveDefaultConfig();
+        $this->getConfig()->reload();
+    }
 
-	public function onLoad(){
-		self::$instance = $this;
-		$this->saveDefaultConfig();
-		$this->getConfig()->reload();
-	}
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getServer()->getCommandMap()->register(Commands::class, new Commands($this));
+        $this->removeAllParticlesInWorlds();
+        foreach (Loader::getInstance()->getConfig()->getAll() as $data) {
+            $ftp = new FakeFloatingTextParticle(new Position($data["x"], $data["y"], $data["z"], Server::getInstance()->getLevelByName($data["levelname"])), $data["text"], $data["title"]);
+            Loader::$particles[$ftp->getEntityId()] = $ftp;
+            Server::getInstance()->getLevelByName($data['levelname'])->addParticle($ftp, Server::getInstance()->getLevelByName($data['levelname'])->getPlayers());
+        }
+    }
 
-	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-		$this->getServer()->getCommandMap()->register(Commands::class, new Commands($this));
-		foreach (Loader::getInstance()->getConfig()->getAll() as $data){
-			$ftp = new FakeFloatingTextParticle(new Position($data["x"], $data["y"], $data["z"], Server::getInstance()->getLevelByName($data["levelname"])), $data["text"], $data["title"]);
-			Loader::$particles[$ftp->getEntityId()] = $ftp;
-			Server::getInstance()->getLevelByName($data['levelname'])->addParticle($ftp, Server::getInstance()->getLevelByName($data['levelname'])->getPlayers());
-		}
-	}
+    public function onDisable()
+    {
+        self::resetConfig();
+        $array = [];
+        foreach (self::$particles as $particle) {
+            $array[] = ["x" => $particle->getX(), "y" => $particle->getY(), "z" => $particle->getZ(), "levelname" => $particle->getLevel()->getName(), "title" => $particle->getTitle(), "text" => $particle->getText()];
+        }
+        $this->getConfig()->setAll($array);
+        $this->getConfig()->save();
+        $this->removeAllParticlesInWorlds();
+    }
 
-	public function onDisable(){
-		self::resetConfig();
-		$array = [];
-		foreach (self::$particles as $particle){
-			$array[] = ["x" => $particle->getX(), "y" => $particle->getY(), "z" => $particle->getZ(), "levelname" => $particle->getLevel()->getName(), "title" => $particle->getTitle(), "text" => $particle->getText()];
-		}
-		$this->getConfig()->setAll($array);
-		$this->getConfig()->save();
-	}
+    private function removeAllParticlesInWorlds(): void
+    {
+        foreach (self::$particles as $particle) {
+            $particle->remove();
+        }
+    }
 
-	/**
-	 * Returns an instance of the plugin
-	 * @return Loader
-	 */
-	public static function getInstance(){
-		return self::$instance;
-	}
+    /**
+     * Returns an instance of the plugin
+     * @return Loader
+     */
+    public static function getInstance()
+    {
+        return self::$instance;
+    }
 }
